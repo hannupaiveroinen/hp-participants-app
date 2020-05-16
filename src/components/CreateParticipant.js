885af21ea3c568a0e17aacf0f3a47901dab6e0f2
Hannu Paiveroinen <hannu.paiveroinen@gmail.com>
2020-05-16 22:50:05 +0100

    form validation

--- src/components/CreateParticipant.js
+++ src/components/CreateParticipant.js
@@ -1,26 +1,34 @@
 import React, { Component } from 'react';
 import { addParticipant } from '../redux/actions';
 import { connect } from "react-redux";
+import ReactFormInputValidation from "react-form-input-validation";
 
 class CreateParticipant extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            name: '',
-            email: '',
-            phone: ''
-        };
+            fields: {
+                name: '',
+                email: '',
+                phone: ''
+            },
+            errors: {}
+        }
 
-        this.handleChange = this.handleChange.bind(this);
+        this.touched = false;
+
+        this.form = new ReactFormInputValidation(this);
+        this.form.useRules({
+            name: "required",
+            email: "required|email",
+            phone: "required|numeric|digits_between:10,12",
+        });
+
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    handleChange(event) {
-        this.setState({ [event.target.name]: event.target.value });
-    }
-
     handleSubmit = () => {
-        this.props.addParticipant(this.state);
+        this.props.addParticipant(this.state.fields);
         this.setState({
             name: '',
             email: '',
@@ -28,15 +36,48 @@ class CreateParticipant extends Component {
         });
     }
 
+    handleTouched = () => {
+        this.touched = true;
+    }
+
     render() {
         return (
             <div className='create-participant-form'>
                 <h1>List of participants</h1>
                 <div>
-                    <input type="text" name="name" value={this.state.name} placeholder="Full name" onChange={this.handleChange} style={{width:190}}/>
-                    <input type="email" name="email" value={this.state.email} placeholder="E-mail address" onChange={this.handleChange}  style={{width:330}}/>
-                    <input type="text" name="phone" value={this.state.phone} placeholder="Phone number" onChange={this.handleChange}  style={{width:190}}/>
-                    <button onClick={this.handleSubmit}>Add new</button>
+                    <input type="text"
+                        name="name"
+                        value={this.state.fields.name}
+                        placeholder="Full name"
+                        onBlur={this.form.handleBlurEvent}
+                        onChange={this.form.handleChangeEvent}
+                        style={{ width: 190 }} />
+                    <input type="email"
+                        name="email"
+                        value={this.state.fields.email}
+                        placeholder="E-mail address"
+                        onBlur={this.form.handleBlurEvent}
+                        onChange={this.form.handleChangeEvent}
+                        style={{ width: 330 }} />
+                    <input type="text"
+                        name="phone"
+                        value={this.state.fields.phone}
+                        placeholder="Phone number"
+                        onBlur={this.form.handleBlurEvent}
+                        onChange={this.form.handleChangeEvent}
+                        style={{ width: 190 }} />
+                    <button disabled={this.state.errors.name || this.state.errors.email || this.state.errors.phone} onClick={this.handleSubmit}>Add new</button>
+                </div>
+                <div style={{ padding: 0 }} >
+                    <label style={{ width: 200 }} className="error">
+                        {this.state.errors.name ? this.state.errors.name : ""}
+                    </label>
+                    <label style={{ width: 340 }} className="error">
+                        {this.state.errors.email ? this.state.errors.email : ""}
+                    </label>
+                    <label style={{ width: 200 }} className="error">
+                        {this.state.errors.phone ? this.state.errors.phone : ""}
+                    </label>
                 </div>
             </div>
         );
